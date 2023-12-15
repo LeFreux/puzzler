b2839af6237777750391f594c8077190fb01fc9a
LeFreux <lefreux@proton.me>
2023-12-15 09:24:25 +0100

    Splitter & drag'n'drop v1

--- main.py
+++ main.py
@@ -16,6 +16,17 @@ class PuzzleGame:
         # Set up the GUI
         self.create_gui(master, rows, cols)
 
+        # Additional attributes for drag-and-drop
+        self.selected_piece = None
+        self.offset_x = 0
+        self.offset_y = 0
+
+        # Bind mouse events
+        self.puzzle_canvas.bind("<ButtonPress-1>", self.on_press)
+        self.puzzle_canvas.bind("<B1-Motion>", self.on_drag)
+        self.puzzle_canvas.bind("<ButtonRelease-1>", self.on_release)
+        self.puzzle_pieces_coordinates = []
+
     def cut_image_in_square(self, rows, cols):
         self.square_width = self.original_image.width // cols
         self.square_height = self.original_image.height // rows
@@ -53,6 +64,27 @@ class PuzzleGame:
                 self.puzzle_canvas.create_image(x0, y0, anchor='nw', image=piece)
 
         self.puzzle_canvas.grid(row=0, column=0, sticky='nsew')
+
+    def on_press(self, event):
+        # Look for the closest canvas item
+        piece_id = self.puzzle_canvas.find_closest(event.x, event.y)
+
+        if piece_id:
+            self.selected_piece = piece_id[0]
+            self.offset_x = event.x - self.puzzle_canvas.coords(self.selected_piece)[0]
+            self.offset_y = event.y - self.puzzle_canvas.coords(self.selected_piece)[1]
+
+    def on_drag(self, event):
+        if self.selected_piece:
+            new_x = event.x - self.offset_x
+            new_y = event.y - self.offset_y
+            self.puzzle_canvas.coords(self.selected_piece, new_x -(self.square_width/2), new_y -(self.square_height/2))
+
+    def on_release(self, event):
+        if self.selected_piece:
+            self.selected_piece = None
+            self.offset_x = 0
+            self.offset_y = 0
 
 if __name__ == "__main__":
     root = CTk.CTk()
